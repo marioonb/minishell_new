94ed7a831ac4daf2a5b183bc81b955250c5fe100
Marion BELORGEY <mbelorge@e2r9p6.42.fr>
2021-01-25 16:27:02 +0100

    attention erreur quote modifie, a revoir

--- srcs/check_errors.c
+++ srcs/check_errors.c
@@ -5,20 +5,39 @@ int	check_error_quote(char *tab, int quote)
 	int	i;
 
 	i = 0;
+	int cpt = 0;
 	while (tab[i])
 	{
-		if (tab[i] == '"' && (i == 0 || tab[i - 1] != '\\'))
+		// AJOUT
+		cpt = 0;
+		while (tab[i] == BACK_S)
+			{
+				cpt++;
+				i++;
+			}
+		//if (tab[i] == DOUBLE_Q && (i == 0 || tab[i - 1] != BACK_S))
+		//{
+		//	quote *= -1;
+		//	while (tab[++i] && quote < 0)
+		//		if (tab[i] == DOUBLE_Q && tab[i - 1] != BACK_S)
+		//			quote *= -1;
+		//}
+		if (tab[i] == DOUBLE_Q && cpt % 2 != 0)
 		{
 			quote *= -1;
 			while (tab[++i] && quote < 0)
-				if (tab[i] == '"' && tab[i - 1] != '\\')
+				{
+					if (tab[i] == BACK_S)
+						cpt ++;
+					if (tab[i] == DOUBLE_Q && cpt % 2 == 0)
 					quote *= -1;
+				}
 		}
-		else if (tab[i] == '\'' && (i == 0 || tab[i - 1] != '\\'))
+		else if (tab[i] == SIMPLE_Q && cpt % 2 != 0) // chang'e avec && (i == 0 || tab[i - 1] != BACK_S)
 		{
 			quote *= -1;
 			while (tab[++i] && quote < 0)
-				if (tab[i] == '\'')
+				if (tab[i] == SIMPLE_Q)
 					quote *= -1;
 		}
 		else
